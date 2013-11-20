b7089ec932d0dbae65206d88ee8e97139357025f
Jerry Chung <jerrychung@lbl.gov>
2013-11-20 15:05:50 -0800

    Squashed 'wp-content/themes/wptimber-biosciences/' changes from 738aba8..b778f28
    b778f28 vendor stuff added
    0eedf9b Made it so onestop menus are visible
    
    git-subtree-dir: wp-content/themes/wptimber-biosciences
    git-subtree-split: b778f289b1950c28c7d50bae5c67503050993d34

--- onestop.php
+++ onestop.php
@@ -6,6 +6,30 @@ Template Name: Onestop Index Page
 use Timber as Timber;
 use TimberMenu as TimberMenu;
 use TimberPost as TimberPost;
+use TimberLoader as TimberLoader;
+
+$pageObj = new Biosciences\Base();
+
+$menus = array('primary', 'footer_left', 'footer_center', 'footer_right');
+$pageObj->find_menus($menus);
+
+$query = array('post_type' => 'page',
+    'post_parent__in' => array(71),
+    'posts_per_page' => -1
+    );
+$pageObj->find_posts($query);
+
+$pageObj->render_page('onestop/index.twig');
+
+/*$context = Timber::get_context();
+$context['menus'] = array();
+
+$menu_ids = array('primary', 'footer_left', 'footer_center', 'footer_right');
+foreach ($menu_ids as $menu_id) {
+    $context['menus'][$menu_id] = new TimberMenu($menu_id);
+}
+
+$context['debug_info'] = "heyo";
 
 $query = array('post_type' => 'page',
     'post_parent__in' => array(71),
@@ -14,4 +38,4 @@ $query = array('post_type' => 'page',
 
 $context = Timber::get_context();
 $context['onestop_posts'] = Timber::get_posts($query);
-Timber::render('views/onestop/index.twig', $context);
+Timber::render('views/onestop/index.twig', $context);*/
