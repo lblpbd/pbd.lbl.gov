3cc0d365d52a672f66f6e50de88b376f96410d9a
Jerry Chung <jerrychung@lbl.gov>
2013-12-09 15:56:18 -0800

    Some image scaling for onestop

---
+++ wp-content/themes/wptimber-biosciences/assets/js/onestop.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    'use strict';
+    var height = $(window).height(),
+        imgHeight = height / 3 - 20;
+
+    $('.onestop__onestop-element img').css('height', imgHeight);
+});
