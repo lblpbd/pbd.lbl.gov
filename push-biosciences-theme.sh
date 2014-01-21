fa6565c14eb3c58431bd19378a590c3f212a4eda
Bryan M. Robles <bryanaka0@gmail.com>
2014-01-21 13:56:04 -0800

    deleted old subtree files, added index.php

--- push-biosciences-theme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true; do
-    read -p "Are you sure you want to push to the theme repository?`echo $'\nYes- Y\nNo - N\n>'`" yn
-    case $yn in
-        [Yy]* ) git subtree push --prefix wp-content/themes/wptimber-biosciences wptimber-biosciences master --squash; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
